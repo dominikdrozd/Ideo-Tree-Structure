624ce4447c65326784b36444fa54d18d56caf8f4
Dominik <dominik.drozd64@gmail.com>
2021-11-25 19:21:33 +0100

    isAdmin middleware that checks if the user is admin.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('nodes', NodeController::class);
+Route::resource('nodes', NodeController::class)->middleware('isAdmin');
 
 Route::get('/', function () {
     return view('welcome');
