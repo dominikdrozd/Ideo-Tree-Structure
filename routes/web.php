9335a9875e4793c112bead4be15692cdc72be528
Dominik <dominik.drozd64@gmail.com>
2021-11-26 08:39:19 +0100

    Fixed bug, that allows to guest to open page with nodes.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('nodes', NodeController::class)->middleware('isAdmin');
+Route::resource('nodes', NodeController::class)->middleware(['isAdmin', 'auth']);
 
 Route::get('/', function () {
     return view('welcome');
